b56732997f60dc958fd05c57df233c2bdd8e9e02
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-09-01 15:12:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blog post app
+
+This is a simple React blog app. The user can add a post, delete a post
+and show the details of a post.
+
+It demonstrates the use of
+* React router
+* redux
+* redux - form
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+import PostIndex from './components/post_index';
+import PostNew from './components/post_new';
+import PostShow from './components/post_show';
+
+export default(
+    <Route path="/" component={App}>
+        <IndexRoute components={PostIndex}/>
+        <Route path="posts/new" component={PostNew}/>
+        <Route path="posts/:id" component={PostShow}/>
+    </Route>
+);
